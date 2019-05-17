6b485ac7dd548cbe2b1841a548853df563ed3573
Marcin <marstan1@onet.pl>
2019-05-17 18:33:23 +0200

    first

---
+++ www/js/main.js
@@ -0,0 +1,134 @@
+﻿(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Obsługa zdarzeń wstrzymywania i wznawiania działania oprogramowania Cordova
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Załadowano oprogramowanie Cordova. Wykonaj tutaj wszystkie wymagane kroki inicjowania tego oprogramowania.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: Ta aplikacja została zawieszona, Zapisz tutaj stan aplikacji.
+    };
+
+    function onResume() {
+        // TODO: Ta aplikacja została ponownie aktywowana. Przywróć tutaj stan aplikacji.
+    };
+})();
+
+// Funkcja obsługuje proces wyświetlenia menu.
+function menuFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+};
+
+// funkcja obsługuje proces kliknięcia w menu wyszukiwania
+function expand() {
+  $(".search").toggleClass("close");
+  $(".input").toggleClass("square");
+  if ($('.search').hasClass('close')) {
+    $('input').focus();
+  } else {
+    $('input').blur();
+  }
+}
+$('button').on('click', expand);
+
+// funkcja ustawia w dropdown wybraną wartość przez użytkownika
+function getItem() {
+    $(".dropdown-menu a").click(function () {
+        $(this).parents(".dropdown").find('.btn').html($(this).text());
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+}
+
+// funkcja filtrujaca wyszukiwarkę.
+function filterSearch() {
+    let filterValue = document.getElementById('filterInput').value.toUpperCase(); // pobranie wartości z wyszukiwarki
+    let ul = document.getElementById('tips'); // pobranie elementu ul
+    let li = ul.querySelectorAll('li.name-item'); // pobranie listy podpowiedzi
+    // Pętla wyświetlająca przefiltrowane podpowiedzi
+    for (let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
+
+// funkcja wyświetla lub nie wyświetla danego diva.
+function displayShow(idElement, bool) {
+    x = document.getElementById(idElement);
+    if (bool === 'true') {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function getData() {
+    $(".form_datetime").datetimepicker({ format: 'yyyy-mm-dd' });
+}
+
+// plik javaScript do definowania funkcji
+// metody objete w funkcjach jss 
+function logIn() {
+    $(".log-in").click(function () {
+        $(".signIn").addClass("active-dx");
+        $(".signUp").addClass("inactive-sx");
+        $(".signUp").removeClass("active-sx");
+        $(".signIn").removeClass("inactive-dx");
+    });
+}
+
+function signIn() {
+    $(".back").click(function () {
+        $(".signUp").addClass("active-sx");
+        $(".signIn").addClass("inactive-dx");
+        $(".signIn").removeClass("active-dx");
+        $(".signUp").removeClass("inactive-sx");
+    });
+}
