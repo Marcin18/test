08ee0fcfc6a33ca05b4093de03de3b328200decd
Marcin <marstan1@onet.pl>
2019-05-21 19:13:29 +0200

    czwarty

--- www/js/main.js
+++ www/js/main.js
@@ -156,22 +156,13 @@ function test() {
         //
         //    xhr.send();
         //}
-
     var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN] = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI] = 'WiFi connection';
-    states[Connection.CELL_2G] = 'Cell 2G connection';
-    states[Connection.CELL_3G] = 'Cell 3G connection';
-    states[Connection.CELL_4G] = 'Cell 4G connection';
-    states[Connection.CELL] = 'Cell generic connection';
-    states[Connection.NONE] = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
-
-
-
+    if (networkState == 'none') { //Connection.NONE   
+        //offline
+        alert("brak internetu");
+    } else {
+        //online  
+        alert("internet");
+    }
 }
 
