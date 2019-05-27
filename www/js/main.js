e8549d28010fa8964af2523971cfe92118b580d4
Marcin <marstan1@onet.pl>
2019-05-27 20:13:42 +0200

    czwarty

--- www/js/main.js
+++ www/js/main.js
@@ -153,12 +153,15 @@ function searchOffer() {
                 var trNav = document.createElement('tr');
                 var thNav = document.createElement('th');
                 thNav.appendChild(document.createTextNode('Kantor'));
+                thNav.setAttribute("style", "text-align:center");
                 trNav.appendChild(thNav);
                 var thNav2 = document.createElement('th');
                 thNav2.appendChild(document.createTextNode('Ulica'));
+                thNav2.setAttribute("style", "text-align:center");
                 trNav.appendChild(thNav2);
                 var thNav3 = document.createElement('th');
                 thNav3.appendChild(document.createTextNode('Kurs'));
+                thNav3.setAttribute("style", "text-align:center");
                 trNav.appendChild(thNav3);
                 tbdy.appendChild(trNav);
                 // konieć dodawania nagłowka tabeli
@@ -168,20 +171,24 @@ function searchOffer() {
                         if (j == 0) {
                             var td = document.createElement('td');
                             td.appendChild(document.createTextNode(dane.rates[step].name));
+                            td.setAttribute("style", "text-align:center");
                             tr.appendChild(td);
                         }
                         if (j == 1) {
                             var td = document.createElement('td');
                             td.appendChild(document.createTextNode(dane.rates[step].street));
+                            td.setAttribute("style", "text-align:center");
                             tr.appendChild(td);
                         }
                         else if (j == 2) {
                             var td = document.createElement('td');
                             if (transactionQuantor === 'sale') {
                                 td.appendChild(document.createTextNode(dane.rates[step].saleValue));
+                                td.setAttribute("style", "text-align:center");
                             }
                             else {
                                 td.appendChild(document.createTextNode(dane.rates[step].purchaseValue));
+                                td.setAttribute("style", "text-align:center");
                             }
                             tr.appendChild(td);
                         }
